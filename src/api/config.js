82477bfa8bc02c25829554ab989f22b5cd365051
freddycordovadd <freddycordovadd@gmail.com>
2018-07-19 18:39:33 -0500

    initial commit

---
+++ src/api/config.js
@@ -0,0 +1,7 @@
+export default {
+    appName: 'platzimusic',
+    apiKey: '90b829a72896b05a3b61529f88f51dc9',
+    secret: '0a146fe773961281919418966dd5b5ba',
+    registeredTo: 'freddycordova'
+}
+
